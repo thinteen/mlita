f44adba0f0bd94f085a996ee4756ba1c534a74e0
thinteen <80061280+thinteen@users.noreply.github.com>
2022-06-05 19:50:42 +0300

    Add files via upload

---
+++ Beetles/Beetles/Beetles/Beetles.cpp
@@ -0,0 +1,115 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void beetlesTrip(const vector<vector<char>>& garMatrix, const int i, const int j, int& actual, const int step)
+{
+	vector<vector<char>> matrix = garMatrix;
+	matrix[i][j] = { '+' };
+
+	if (garMatrix[i + 1][j] == '#')
+	{
+		if (actual < step + 1)
+		{
+			actual = step + 1;
+		}	
+		beetlesTrip(matrix, i + 1, j, actual, step + 1);
+	}
+
+	if (garMatrix[i - 1][j] == '#')
+	{
+		if (actual < step + 1)
+		{
+			actual = step + 1;
+		}
+		beetlesTrip(matrix, i - 1, j, actual, step + 1);
+	}
+
+	if (garMatrix[i][j + 1] == '#')
+	{
+		if (actual < step + 1)
+		{
+			actual = step + 1;
+		}
+		beetlesTrip(matrix, i, j + 1, actual, step + 1);
+	}
+
+	if (garMatrix[i][j - 1] == '#')
+	{
+		if (actual < step + 1)
+		{
+			actual = step + 1;
+		}	
+		beetlesTrip(matrix, i, j - 1, actual, step + 1);
+	}
+}
+
+int main()
+{
+	ofstream outputFile("output.txt");
+	ifstream inputFile("input.txt");
+	if (!inputFile.is_open())
+		return 1;
+
+	int m, n, result = 0, potatoesCount = 0;
+	char potato;
+
+	inputFile >> m >> n;
+
+	if ((m > 5) or (n > 5))
+	{
+		outputFile << "Square of garden is more than it can be";
+		exit(0);
+	}
+	
+	if ((m < 5) or (n < 5))
+	{
+		outputFile << "Square of garden is less than it can be";
+		exit(0);
+	}
+
+	vector<vector<char>> garMatrix(m + 2, vector<char>(n + 2, '.'));
+
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			inputFile >> potato;
+			if (potato == '#')
+			{
+				garMatrix[i + 1][j + 1] = potato;
+			}		
+		}
+	}
+
+	for (size_t i = 0; i < garMatrix.size(); i++)
+	{
+		for (size_t j = 0; j < garMatrix[0].size(); j++)
+		{
+			if (garMatrix[i][j] == '#') potatoesCount++;
+		}
+	}
+
+	for (size_t i = 0; i < garMatrix.size(); i++)
+	{
+		for (size_t j = 0; j < garMatrix[0].size(); j++)
+		{
+			if (garMatrix[i][j] == '#')
+			{
+				int actual = 1;
+				beetlesTrip(garMatrix, i, j, actual, 1);
+				result = max(result, actual);
+				if (result == potatoesCount)
+				{
+					outputFile << result;
+					exit(0);
+				}
+			}
+		}
+	}
+
+	outputFile << result;
+	return 0;
+}
---
+++ Otrezki/Otrezki/Otrezki.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = INT_MAX;
+
+const int SIZE = 7;
+int a[] = { 5, 3, 2, 4, 1, 6, 7 };
+vector<int> treeMax(4 * SIZE);
+vector<int> treeSum(4 * SIZE);
+
+void buildMaxTree(int v, int L, int R)
+{
+	if (L == R - 1)
+	{
+		if (L < SIZE)
+			treeMax[v] = a[L];
+		return;
+	}
+	int M = (L + R) / 2;
+	buildMaxTree(2 * v + 1, L, M);
+	buildMaxTree(2 * v + 2, M, R);
+	treeMax[v] = max(treeMax[2 * v + 1], treeMax[2 * v + 2]);
+}
+
+void buildSumTree(int v, int L, int R)
+{
+	if (L == R - 1)
+	{
+		if (L < SIZE)
+			treeSum[v] = a[L];
+		return;
+	}
+	int M = (L + R) / 2;
+	buildSumTree(2 * v + 1, L, M);
+	buildSumTree(2 * v + 2, M, R);
+	treeSum[v] = treeSum[2 * v + 1] + treeSum[2 * v + 2];
+}
+
+int rmq(int v, int L, int R, int l, int r)
+{
+	if (r <= L || R <= l) 
+		return 0;
+	if (l <= L && R <= r) 
+		return treeMax[v];
+	int M = (L + R) / 2;
+	return max(rmq(2 * v + 1, L, M, l, r), rmq(2 * v + 2, M, R, l, r));
+}
+
+void Add1(int v, int L, int R, int i, int x)
+{
+	if (L == R - 1)
+	{
+		treeSum[v] += x;
+		treeMax[v] += x;
+		return;
+	}
+	
+	int M = (L + R) / 2;
+
+	if (i < M) 
+		Add1(2 * v + 1, L, M, i, x);
+	else 
+		Add1(2 * v + 2, M, R, i, x);
+
+	treeSum[v] = treeSum[2 * v + 1] + treeSum[2 * v + 2];
+	treeMax[v] = max(treeMax[2 * v + 1], treeMax[2 * v + 2]);
+}
+
+void ReverseSegmentsTree()
+{
+	cout << "\n";
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cout << rmq(0, 0, SIZE, i, i + 1) << " ";
+	}
+}
+
+int main()
+{
+	buildMaxTree(0, 0, SIZE);
+	buildSumTree(0, 0, SIZE);
+
+	cout << "Select an operation: \n";
+	cout << "\t1 - Rmq\n\t2 - Add1\n\t3 - Add2\n\t0 - exit";
+
+	int i = 0, j = 0, k = 0, x = 0;
+
+	bool exit = false;
+	while (!exit)
+	{
+		cout << "\n< operation: ";
+		cin >> i;
+		switch (i)
+		{
+		case 0:
+			exit = true;
+			break;
+		case 1:
+			cin >> k;
+			cin >> j;
+			cout << rmq(0, 0, SIZE, k, j);
+			break;
+		case 2:
+			cin >> k;
+			cin >> j;
+			Add1(0, 0, SIZE, k - 1, j);
+			ReverseSegmentsTree();
+			break;
+		case 3:
+			break;
+		}
+	}
+	return 0;
+}
